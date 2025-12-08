de248635242e49ef4687346886641e51396a9142
Martin987dev <lempnerm@kkszki.hu>
2025-12-08 09:07:52 +0100

    Models

---
+++ EtteremApi/Models/EtelekContext.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace EtteremApi.Models;
+
+public partial class EtelekContext : DbContext
+{
+    public EtelekContext()
+    {
+    }
+
+    public EtelekContext(DbContextOptions<EtelekContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Kapcsolo> Kapcsolos { get; set; }
+
+    public virtual DbSet<Renddele> Renddeles { get; set; }
+
+    public virtual DbSet<Termekek> Termekeks { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseMySQL("server=localhost;database=etelek;user=root;password=;");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Kapcsolo>(entity =>
+        {
+            entity.HasKey(e => e.KapcsoloId).HasName("PRIMARY");
+
+            entity.ToTable("kapcsolo");
+
+            entity.HasIndex(e => e.RendelesId, "RendelesId");
+
+            entity.HasIndex(e => e.TermekekId, "TermekekId");
+
+            entity.Property(e => e.KapcsoloId).HasColumnType("int(11)");
+            entity.Property(e => e.RendelesId).HasColumnType("int(11)");
+            entity.Property(e => e.TermekekId).HasColumnType("int(11)");
+
+            entity.HasOne(d => d.Rendeles).WithMany(p => p.Kapcsolos)
+                .HasForeignKey(d => d.RendelesId)
+                .HasConstraintName("kapcsolo_ibfk_2");
+
+            entity.HasOne(d => d.Termekek).WithMany(p => p.Kapcsolos)
+                .HasForeignKey(d => d.TermekekId)
+                .OnDelete(DeleteBehavior.Restrict)
+                .HasConstraintName("kapcsolo_ibfk_1");
+        });
+
+        modelBuilder.Entity<Renddele>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PRIMARY");
+
+            entity.ToTable("renddeles");
+
+            entity.Property(e => e.Id).HasColumnType("int(11)");
+            entity.Property(e => e.Asztalszam)
+                .HasDefaultValueSql("'NULL'")
+                .HasColumnType("int(11)");
+            entity.Property(e => e.Fizetesimod)
+                .HasMaxLength(50)
+                .HasDefaultValueSql("'NULL'");
+        });
+
+        modelBuilder.Entity<Termekek>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PRIMARY");
+
+            entity.ToTable("termekek");
+
+            entity.Property(e => e.Id).HasColumnType("int(11)");
+            entity.Property(e => e.Ar)
+                .HasDefaultValueSql("'NULL'")
+                .HasColumnType("int(11)");
+            entity.Property(e => e.Etel)
+                .HasMaxLength(255)
+                .HasDefaultValueSql("'NULL'");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ EtteremApi/Models/Kapcsolo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EtteremApi.Models;
+
+public partial class Kapcsolo
+{
+    public int KapcsoloId { get; set; }
+
+    public int RendelesId { get; set; }
+
+    public int TermekekId { get; set; }
+
+    public virtual Renddele Rendeles { get; set; } = null!;
+
+    public virtual Termekek Termekek { get; set; } = null!;
+}
